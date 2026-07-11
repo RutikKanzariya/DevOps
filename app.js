6d9aa0559d14da33d259198a957d52106e14f88a
RutikKanzariya <rutikkanjariya@gmail.com>
2026-07-11 13:49:16 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+let express = require('express')
+let app = express()
+let cors = require('cors')
+let port = 9000
+
+app.use(cors());
+
+app.get('/ping',(req,res) => {
+    res.send({
+        message:"Hello world!"
+    });
+})
+
+app.listen(port,()=>{
+    console.log(`Server is running on http://localhost:${port}`)
+
+});
